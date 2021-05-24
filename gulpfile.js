21e275a896ebe1ec8d4246c60760630a675dc4c4
nrrozhkov <nrrozhkov@gmail.com>
2021-05-25 00:33:00 +0300

    Исправил ошибки, оптимизировал шрифты

--- gulpfile.js
+++ gulpfile.js
@@ -77,7 +77,7 @@ exports.images = copyImages;
 // WebP
 
 const createWebp = () => {
-  return gulp.src("source/img/**/*.{jpg,png}")
+  return gulp.src("source/img/*.{jpg,png}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("build/img"))
 }
